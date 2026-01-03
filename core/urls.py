252f947626a2be86b5548b25c246840b100f3324
Alabsgold <alabsgold31@gmail.com>
2026-01-03 11:19:09 +0100

    urls.py update

--- core/urls.py
+++ core/urls.py
@@ -28,18 +28,6 @@ urlpatterns = [
     path('teacher/quiz/<int:quiz_id>/add-question/', views.add_question_to_quiz, name='add_question'),
     path('teacher/search-quizzes/', views.search_quizzes, name='search_quizzes'),
     
-    # These teacher URLs below might need to be updated or removed now that we have the formset.
-    # We can review them later.
-    # path('teacher/quiz/<int:quiz_id>/', views.quiz_detail, name='quiz_detail'),
-    # path('teacher/question/<int:question_id>/edit/', views.edit_question, name='edit_question'),
-
-
-    # --- Student URLs ---
-    path('student/dashboard/', views.student_dashboard, name='student_dashboard'),
-    path('student/quiz/<int:quiz_id>/take/', views.take_quiz, name='take_quiz'),
-    
     # ALABI'S NOTE: Corrected this URL to pass the result_id, which the view now requires.
     path('student/result/<int:result_id>/', views.quiz_result, name='quiz_result'),
-
-    # path('student/quiz/<int:quiz_id>/review/', views.review_quiz, name='review_quiz'),
 ]
